0d8f33ac194872886e0c159224c226b5aebacb8a
ForJadeForest <yingzhepeng@foxmail.com>
2023-05-22 19:57:06 +0800

    add setup

--- setup.py
+++ setup.py
@@ -4,8 +4,16 @@ from setuptools import setup, find_packages
 setup(
     name='minigpt4',
     version='0.1',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
     description='The package for minigpt4',
     author='YingzhePeng',
     author_email='yingzhepeng@foxmail.com',
-    packages=find_packages()
+    packages=find_packages(),
 )
