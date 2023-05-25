5204a512e87e4df34c6c5cd22cf381b62de30c3d
Zhijie Lin <136380243@qq.com>
2023-05-25 15:35:59 +0800

    test

--- demo.py
+++ demo.py
@@ -54,6 +54,7 @@ cfg = Config(args)
 model_config = cfg.model_cfg
 model_config.device_8bit = args.gpu_id
 model_cls = registry.get_model_class(model_config.arch)
+print(model_config)
 model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
 
 # TODO: Fix hard-coding  `cc12m`
