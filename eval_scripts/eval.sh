f91ef5f724048fc1b184d10168d720971063f676
Xiang Li <44761952+lx709@users.noreply.github.com>
2023-10-27 14:10:22 +0300

    Update eval.sh

--- eval_scripts/eval.sh
+++ eval_scripts/eval.sh
@@ -8,12 +8,12 @@ NAME=EXP_NAME
 IMG_PATH=YOUR_IMG_PATH
 EVAL_FILE_PATH=YOUR_EVAL_FILE_PATH
 
-torchrun --nproc_per_node 1 eval_ref.py --name ${NAME} \
+torchrun --nproc_per_node 1 eval_scripts/eval_ref.py --name ${NAME} \
  --cfg-path ${cfg_path} \
  --ckpt ${CKPT} --dataset refcoco,refcoco+,refcocog --lora_r 64 --lora_alpha 16 \
  --batch_size 64 --max_new_tokens 20 --resample --img_path ${IMG_PATH} --eval_file_path ${EVAL_FILE_PATH}
  
-torchrun --nproc_per_node 1 eval_vqa.py --name ${NAME} \
+torchrun --nproc_per_node 1 eval_scripts/eval_vqa.py --name ${NAME} \
  --cfg-path ${cfg_path} \
  --ckpt ${CKPT} --split val,test --dataset okvqa,vizwiz,aokvqa,iconqa,gqa,vsr,hm --lora_r 64 --lora_alpha 16 \
  --batch_size 32 --max_new_tokens 20 --resample
