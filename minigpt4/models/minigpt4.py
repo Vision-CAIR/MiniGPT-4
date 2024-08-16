8709e9966df9b1cde46a67f1a6c3b527022675fe
3luka <94873742+MohamedAlaaAli@users.noreply.github.com>
2024-08-16 15:33:52 +0300

    Update minigpt4.py

--- minigpt4/models/minigpt4.py
+++ minigpt4/models/minigpt4.py
@@ -189,7 +189,7 @@ class MiniGPT4(MiniGPTBase):
         ckpt_path = cfg.get("ckpt", "")  # load weights of MiniGPT-4
         if ckpt_path:
             print("Load MiniGPT-4 Checkpoint: {}".format(ckpt_path))
-            ckpt = torch.load(ckpt_path, map_location="cuda")
+            ckpt = torch.load(ckpt_path, map_location="cuda" if torch.cuda.is_available() else "cpu")
             msg = model.load_state_dict(ckpt['model'], strict=False)
 
         return model
